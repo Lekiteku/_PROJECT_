68bb39bb9736f7639d3adb5d110c0eee0c7eeaa9
John Lbalunye <jlbalunye7@gmail.com>
2023-10-28 00:54:38 +0300

    update

--- servermain.py
+++ servermain.py
@@ -1,43 +1,68 @@
-import socket
 import cv2
 import numpy as np
+import socket
+import queue
+import threading
+import time
+import struct
 
-# Create a socket to listen for client connections
-server_ip = '0.0.0.0'  # Listen on all available network interfaces
-server_port = 12345  # Use the same port as the client
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((server_ip, server_port))
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print('Waiting for client connection...')
+# Bind the socket to an IP and port to receive video frames
+receiver_ip = '0.0.0.0'  # Listen on all available network interfaces
+receiver_port = 12345  # Use the same port as in the sender
+
+try:
+    client_socket.bind((receiver_ip, receiver_port))
+except Exception as e:
+    print(f"Error binding to socket: {e}")
+    exit(1)
+
+# Create a VideoWriter to decode and display the frames
+fourcc = cv2.VideoWriter_fourcc(*'H264')
+out = cv2.VideoWriter('receiver_output.mp4', fourcc, 20.0, (640, 480))  # Adjust as needed
+
+# Initialize a jitter buffer
+frame_buffer = queue.PriorityQueue()
+
+def process_frame(frame_data, timestamp):
+    frame = cv2.imdecode(np.frombuffer(frame_data, np.uint8), cv2.IMREAD_COLOR)
+    out.write(frame)
+    cv2.imshow("Received", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        return
 
 while True:
-    # Receive the data size indicating the frame length
-    data_size = int.from_bytes(server_socket.recv(4), byteorder='big')
+    try:
+        data, addr = client_socket.recvfrom(65535)  # Adjust buffer size if needed
+    except Exception as e:
+        print(f"Error receiving data: {e}")
+        continue
 
-    # Receive the frame data
-    data = b''
-    while len(data) < data_size:
-        packet = server_socket.recv(data_size - len(data))
-        if not packet:
-            break
-        data += packet
+    try:
+        # Deserialize the frame from bytes
+        timestamp, frame_data = data[:8], data[8:]
+        frame_timestamp = struct.unpack('d', timestamp)[0]
 
-    # Log the received frame size
-    print(f'Received frame size: {data_size} bytes')
+        # Place the frame in the buffer with its timestamp as the priority
+        frame_buffer.put((frame_timestamp, frame_data))
+    except Exception as e:
+        print(f"Error processing frame: {e}")
+        continue
 
-    # Convert binary data to a NumPy array
-    frame_data = np.frombuffer(data, dtype=np.uint8)
+    # Process frames in the buffer
+    current_time = time.time()
+    while not frame_buffer.empty():
+        frame_timestamp, frame_data = frame_buffer.get()
+        frame_age = current_time - frame_timestamp
 
-    # Decode the JPEG image
-    frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
+        # Process the frame if it's not too old (adjust the threshold as needed)
+        if frame_age < 1.0:
+            process_frame(frame_data, timestamp)
+        else:
+            print(f"Discarded an old frame (age: {frame_age:.2f} seconds)")
 
-    if frame is not None:
-        # Optionally, you can save or process the JPEG image here
-        cv2.imwrite('received_image.jpg', frame)  # Save the received image
-
-        # Perform any further processing or display as needed
-        cv2.imshow('Received Image', frame)
-        cv2.waitKey(0)
-
-# Don't forget to release the OpenCV window when done
+# Release resources when done
+out.release()
 cv2.destroyAllWindows()
