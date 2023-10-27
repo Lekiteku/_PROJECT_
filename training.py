0150701501e992bd041b5edfeaffaa3f963eb76c
John Lbalunye <jlbalunye7@gmail.com>
2023-10-27 13:08:17 +0300

    testing

--- training.py
+++ training.py
@@ -9,11 +9,7 @@ haar_cascade = cv.CascadeClassifier('haar_face.xml')
 DIR = r'dataset'
 
 # Create an empty list to store the names of individuals
-people = []
-
-# Loop through subdirectories and add names to the 'people' list
-for i in os.listdir(DIR):
-    people.append(i)
+people = sorted(os.listdir(DIR))  # Ensure alphabetical sorting
 
 # Create empty lists to store training data and labels
 features = []
@@ -28,18 +24,19 @@ def create_train():
         for img in os.listdir(path):
             img_path = os.path.join(path, img)
             img_array = cv.imread(img_path)
-            
-            # Detect faces in the image
-            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
-            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
-            
-            for (x, y, w, h) in faces_rect:
-                faces_roi = gray[y:y+h, x:x+w]
-                # Resize to a standard size (e.g., 128x128) and normalize pixel values
-                faces_roi = cv.resize(faces_roi, (128, 128)) / 255.0
-                
-                features.append(faces_roi)
-                labels.append(label)
+
+            if img_array is not None:
+                # Detect faces in the image
+                gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+                faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+                for (x, y, w, h) in faces_rect:
+                    faces_roi = gray[y:y+h, x:x+w]
+                    # Resize to a standard size (e.g., 128x128) and normalize pixel values
+                    faces_roi = cv.resize(faces_roi, (128, 128)) / 255.0
+
+                    features.append(faces_roi)
+                    labels.append(label)
 
 # Call the 'create_train()' function to prepare the training data
 create_train()
@@ -49,7 +46,7 @@ features = np.array(features, dtype=np.float32)
 labels = np.array(labels)
 
 # Create LBPH (Local Binary Pattern Histogram) face recognizer
-face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer = cv.face_LBPHFaceRecognizer_create()
 
 # Train the face recognizer using the prepared 'features' and 'labels' data
 face_recognizer.train(features, labels)
@@ -62,4 +59,4 @@ np.save('features.npy', features)
 np.save('labels.npy', labels)
 
 # Print a message to indicate that the training is finished
-print('Finish training...')
+print('Training completed.')
