3d91d6f0416ac75c32fb370a0f053e50d82bf161
John Lbalunye <jlbalunye7@gmail.com>
2023-11-14 22:28:39 +0300

    Updated Communication

--- communication.py
+++ communication.py
@@ -5,10 +5,41 @@ This code defines the CommunicationManager class, a utility for managing parent 
 
 Class Methods:
 """
+import socket
+import json
 import numpy as np
 
 class CommunicationManager:
     
+    HOST_IP = '192.168.0.6'
+
+    VIDEO_PORT = 9999
+    VIDEO_BUFF_SIZE = 65536
+    VIDEO_CLIENT_ADDRESS = ""
+    video_client_socket = None
+
+    STATUS_PORT = 8888
+    STATUS_BUFF_SIZE = 1024
+    STATUS_CLIENT_ADDRESS = ""
+    status_client_socket = None
+
+    LOCATION_PORT = 7777
+    LOCATION_BUFF_SIZE = 1024
+    LOCATION_CLIENT_ADDRESS = ""
+    location_client_socket = None
+
+
+    DISPLAY_PORT = 7777
+    DISPLAY_BUFF_SIZE = 1024
+    DISPLAY_CLIENT_ADDRESS = ""
+    display_client_socket = None
+
+
+    SMS_PORT = 7777
+    SMS_BUFF_SIZE = 1024
+    SMS_CLIENT_ADDRESS = ""
+    sms_client_socket = None
+
 
     used_location_set = set()
     """
@@ -21,14 +52,14 @@ class CommunicationManager:
     
     It checks for available location indexes, finds the unused indexes, retrieves parent information for those indexes, and returns it.
     """
-    @staticmethod
-    def get_parent_info_by_location_index(clr,parent_info, location_indexes):
+    @classmethod
+    def get_parent_info_by_location_index(cls,parent_info, location_indexes):
         if location_indexes.size == 0:
             print("The 'location_indexes' array is empty.")
             return None
 
         # Find the unused indexes using NumPy set operations
-        unused_indexes = np.setdiff1d(location_indexes, list(used_indexes))
+        unused_indexes = np.setdiff1d(location_indexes, list(cls.used_indexes))
 
         if unused_indexes.size == 0:
             print("All location indexes have been used.")
@@ -67,9 +98,115 @@ class CommunicationManager:
     This static method takes a single parameter, used_indexes, which is a set that stores the used indexes. It clears the set of used indexes, effectively resetting it.
     """
     @staticmethod
-    def reset_used_indexes(used_indexes):
-        used_indexes.clear()
+    def reset_used_indexes(cls):
+        cls.used_indexes.clear()
 
-"""
-The class is designed to be used without creating an instance. It separates the operations from the class instance, making it a useful utility for managing parent information without the need for class instantiation. The class itself doesn't have any instance attributes or methods that depend on the class state; all operations are performed based on the parameters provided to the static methods.
-"""
+    @classmethod
+    def start_video_server(cls):
+        cls.video_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        cls.video_client_socket.bind((cls.HOST_IP, cls.VIDEO_PORT))
+        print("Establishing a connection with Video Server")
+        while True:
+        # Initial connection message to the server
+            cls.video_client_socket.sendto(b'1', (cls.HOST_IP,cls.VIDEO_PORT))
+            data, server_address = cls.video_client_socket.recvfrom(cls.VIDEO_BUFF_SIZE)
+            print("Connection established with the Video Server.")
+
+
+    @classmethod
+    def receive_video_data(cls):
+        data =cls.video_client_socket.recvfrom(cls.VIDEO_BUFF_SIZE)
+        return data
+
+    @classmethod
+    def stop_video_server(cls,):
+        cls.video_client_socket.close()
+
+
+    @classmethod
+    def start_status_server(cls):
+        cls.status_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        cls.status_client_socket.bind((cls.HOST_IP, cls.STATUS_PORT))
+        print("Establishing a connection with Status Server")
+
+        while True:
+        # Initial connection message to the server
+            cls.video_client_socket.sendto(b'2', (cls.HOST_IP,cls.STATUS_PORT))
+            data, server_address = cls.video_client_socket.recvfrom(cls.STATUS_BUFF_SIZE)
+            print("Connection established with the Status Server.")
+
+
+    @classmethod
+    def receive_status_data(cls):
+        data = cls.status_client_socket.recvfrom(cls.STATUS_BUFF_SIZE)
+        return data
+
+    @classmethod
+    def stop_status_server(cls):
+        cls.status_client_socket.close()
+
+    @classmethod
+    def start_location_server(cls):
+        cls.location_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        cls.location_client_socket.bind((cls.HOST_IP, cls.LOCATION_PORT))
+        print("Establishing a connection with Location Server")
+
+        while True:
+        # Initial connection message to the server
+            cls.location_client_socket.sendto(b'3', (cls.HOST_IP,cls.LOCATION_PORT))
+            data, server_address = cls.location_client_socket.recvfrom(cls.LOCATION_BUFF_SIZE)
+            print("Connection established with the Location Server.")
+
+    @classmethod
+    def receive_location_data(cls):
+        cls.location_client_socket.recvfrom(cls.LOCATION_BUFF_SIZE)
+
+    @classmethod
+    def stop_location_server(cls):
+        cls.location_client_socket.close()
+
+    @classmethod
+    def start_display_server(cls):
+        cls.display_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cls.display_client_socket.bind((cls.HOST_IP, cls.DISPLAY_PORT))
+
+        print("Establishing a connection with Display Server")
+
+        while True:
+        # Initial connection message to the server
+            cls.display_client_socket.sendto(b'4', (cls.HOST_IP,cls.DISPLAY_PORT))
+            data, server_address = cls.display_client_socket.recvfrom(cls.DISPLAY_BUFF_SIZE)
+            print("Connection established with the Display Server.")
+
+
+    @classmethod
+    def send_display_data(cls,message):
+        cls.display_client_socket.sendall(message)
+    
+
+    @classmethod
+    def stop_display_server(cls):
+        cls.display_client_socket.close()
+
+
+    @classmethod
+    def start_sms_server(cls):
+        cls.sms_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        cls.sms_client_socket.bind((cls.HOST_IP, cls.SMS_PORT))
+        
+        print("Establishing a connection with SMS Server")
+
+        while True:
+        # Initial connection message to the server
+            cls.display_client_socket.sendto(b'5', (cls.HOST_IP,cls.SMS_PORT))
+            data, server_address = cls.display_client_socket.recvfrom(cls.SMS_BUFF_SIZE)
+            print("Connection established with the SMS Server.")
+
+
+    @classmethod
+    def send_sms_data(cls,message):
+        cls.display_client_socket.sendall(message)
+
+    @classmethod
+    def stop_display_server(cls):
+        cls.sms_client_socket.close()
